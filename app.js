a5a97c6f7bfa6fc106a56f11a04e62dde78d3b85
Aaditya Jagtap <aadityajagtap2205@gmail.com>
2025-07-19 13:48:56 +0530

    Final Push

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const PORT = 8080;
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ALTASDB_URL;
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
@@ -15,21 +15,34 @@ const listing_route = require("./routes/listing_route.js");
 const review_route = require('./routes/review_route.js');
 const user_route = require('./routes/user_route.js');
 const session = require("express-session");
+const MongoStore = require('connect-mongo');
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStratergy = require("passport-local");
 const User = require("./models/user.js");
 
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  crypto: {
+    secret: process.env.SECRET,
+  },
+  touchAfter: 24 * 3600,
+});
+
+store.on("error" , ()=>{
+  console.log("ERROR ON MONGO SESSION STORE" , err)
+})
 
 const sessionOptions = {
-   secret: "mysecretcode",
-   resave: false,
-   saveUninitialized: true,
-   cookie:{
+  store,
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie:{
     expires: Date.now() + 1000 * 60 * 60 * 24 * 3,
     maxAge: 1000 * 60 * 60 * 24 * 3,
     httpOnly: true
-   }
+  }
 }
 
 app.set("view engine", "ejs");
@@ -54,6 +67,10 @@ passport.use(new LocalStratergy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// mongoose.connect(process.env.ALTASDB_URL)
+//   .then(() => console.log('Connected to MongoDB Atlas'))
+//   .catch(err => console.error('MongoDB connection error:', err));
+
 main()
   .then((res) => {
     console.log("Connection succesful...");
@@ -61,7 +78,7 @@ main()
   .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
 }
 
 app.use((req , res , next) =>{
